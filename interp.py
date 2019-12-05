503e192a392a26073ddec9674929d46b76afd05b
BaurTokenov <mid1or1feed@gmail.com>
2019-12-05 16:06:25 +0900

    Done Pointers
    Pointer dereferencing, getting addresses of variables.

--- interp.py
+++ interp.py
@@ -8,6 +8,7 @@ history_table = {} #Str -> List[Val] #saves history of a variable
 memory_table = [] #Int -> Val 
 
 symbol_table_stack = [] #stack of the symbol_tables, for implementing scopes;
+history_table_stack = [] #stack of the history tables, used for showing right history, according to the scope
 addr_stack = [] #stack of nodes, used for implementing function call and return.
 rax = 0 #function return values stored here
 current_linenode = None
@@ -33,17 +34,31 @@ class LineNode:
 class Assignment:
 	def __init__(self, var, expr):
 		self.var = var
+		print('SElf.var', self.var)
 		self.expr = expr
 
 	def __str__(self):
-		return 'Assignment' + ' ' + self.var
+		return 'Assignment' + ' ' + str(self.var)
 
 	def process(self):
-		if self.var not in symbol_table:
-			not_declared_error(self.var)
+		if self.var[1] not in symbol_table:
+			not_declared_error(self.var[1])
 		else:
-			memory_table[symbol_table[self.var]] = evaluate(self.expr)
-			history_table[self.var].append(memory_table[symbol_table[self.var]])
+			if self.var[0] == None:#regular case, not a pointer
+				eval_res = evaluate(self.expr)
+				memory_table[symbol_table[self.var[1]]] = eval_res
+				history_table[self.var[1]].append(memory_table[symbol_table[self.var[1]]])
+			else:#pointer case with a star in front of it
+				eval_res = evaluate(self.expr)
+				pointer_val = memory_table[symbol_table[self.var[1]]]	
+				if isinstance(pointer_val, Pointer) and pointer_val.addr != None and pointer_val.addr < len(memory_table): 
+					memory_table[pointer_val.addr] = eval_res
+				else:
+					if isinstance(pointer_val, Pointer):
+						invalid_pointer_error(pointer_val)
+					else:
+						not_a_pointer_error(pointer_val)
+
 		get_to_next_linenode()
 
 
@@ -61,6 +76,9 @@ class Expression:
 def get_expression(expr_dict):
 	if isinstance(expr_dict, str):
 		return Expression(None, expr_dict, None);
+	if isinstance(expr_dict, tuple):
+		print(expr_dict);
+		return Expression(None, expr_dict, None)
 	if isinstance(expr_dict, dict):
 		left = expr_dict['left']
 		right = expr_dict['right']
@@ -76,12 +94,22 @@ class Declaration:
 		return 'Declaration:' + ' ' + self.vartype + ' ' + str(self.variables);
 	def process(self):
 		for var in self.variables:
-			symbol_table[var] = len(memory_table);
-			memory_table.append('N/A');
-			history_table[var] = ['N/A']
+			#var = [null, name]
+			symbol_table[var[1]] = len(memory_table);
+			history_table[var[1]] = ['N/A']
+			if var[0] == None:
+				#symbol_table[var[1]] = len(memory_table);
+				memory_table.append('N/A');
+			else:
+				memory_table.append(Pointer(None));
 		get_to_next_linenode();
 
 
+class Pointer:
+	def __init__(self, addr):
+		self.addr = addr
+	def __str__(self):
+		return 'Pointer with addr: ' + str(self.addr)
 
 class IfClause:
 	def __init__(self, expr, body):
@@ -100,13 +128,16 @@ class IfClause:
 		if self.visited:
 			print("it was visited")
 			global symbol_table
+			global history_table
 			symbol_table = symbol_table_stack.pop();
+			history_table = history_table_stack.pop();
 			get_to_next_linenode();
 			return;
 		cond_expr_val = evaluate(self.expr);
 		if cond_expr_val:
 			global current_linenode
 			symbol_table_stack.append(symbol_table.copy())
+			history_table_stack.append(history_table.copy())
 			self.visited = True
 			current_linenode = self.body[0]
 		else:
@@ -115,18 +146,39 @@ class IfClause:
 
 #looks up the symbol table or tells that there's no such variable
 def lookup_value(expr):
-	print("Looking up " + expr);
-	print(symbol_table);
-	print(expr in symbol_table);
 	if expr in symbol_table:
 		return memory_table[symbol_table[expr]]
 	else:
 		not_declared_error(expr);
 
+#dereference pointer
+def deref_pointer(pointer):
+	if isinstance(pointer, Pointer) and pointer.addr != None and pointer.addr < len(memory_table): 
+		return memory_table[pointer.addr]
+	else:
+		if isinstance(pointer, Pointer):
+			invalid_pointer_error(pointer.addr)
+		else:
+			not_a_pointer_error(pointer)
+
+
+#get addr of a variable
+def get_var_address(var):
+	if var in symbol_table:
+		return symbol_table[var]
+	else:
+ 		not_declared_error(var)
 
 #function for showing errors about undefined variables
 def not_declared_error(var):
 	print("Variable {} is not declared".format(var))
+
+def invalid_pointer_error(addr):
+	print("Access to invalid address: {}".format(addr)) 
+
+def not_a_pointer_error(var):
+	print("The variable {} is not a pointer".format(var))
+
 
 
 def is_float(expr):
@@ -143,13 +195,19 @@ def is_string(expr):
 
 #Evaluates expression to some value
 def evaluate(expr):
+	#print("evaluating", expr)
 	if isinstance(expr, str):
 		if expr.isdigit():
 			return int(expr)
 		if is_float(expr):
 			return float(expr)
-		else:
-			return lookup_value(expr);
+	if isinstance(expr, tuple):
+		if expr[0] == None:
+			return lookup_value(expr[1])
+		elif expr[0] == '*':
+			return deref_pointer(lookup_value(expr[1]));
+		elif expr[0] == '&':
+			return Pointer(get_var_address(expr[1]))
 	if isinstance(expr, Expression):
 		op = expr.op
 		if op != None:
@@ -188,11 +246,13 @@ def get_op(line_info):
 		expr = line_info['value']['expression']
 		return Assignment(var, get_expression(expr))
 	if line_info['type'] == 'declaration':
+		is_pointer = line_info['value']['variables'][0] == '*' 		
 		variables = line_info['value']['variables']
 		vartype = line_info['value']['type'];
 		return Declaration(vartype, variables);
 	if line_info['type'] == 'if_clause':
 		expr = line_info['value']['expr']
+		print('expression inside if clause', expr)
 		body = get_flow_graph(line_info['value']['body']) # we have end and start of if clause here
 		#print("If body start and end", end = ': ')
 		#print(body[0], body[1])
@@ -200,7 +260,7 @@ def get_op(line_info):
 
 
 def get_flow_graph(body):
-	line_prev = None
+	line_prev = None 
 	line_start = None;
 	for line_info in body:
 		lineno = line_info['span'][0];
@@ -295,7 +355,8 @@ def start_interpreter():
 		print('Symbol table stack', symbol_table_stack);
 		print('Memory table', memory_table);
 		print('History table', history_table);
-		print(user_cmd);
+		print('History table stack', history_table_stacks)
+		#print(user_cmd);
 		if len(user_cmd) == 0:
 			continue;
 		if user_cmd[0] == 'print' or user_cmd[0] == 'p':
